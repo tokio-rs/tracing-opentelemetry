94fddb035348c651989fa3d66cdc34099f186d2c
Cyrill Bucher <cyrill.bucher@meintest.software>
2025-12-08 16:46:40 +0100

    test: add test for mutlithreaded follows from panic

--- tests/follows_from.rs
+++ tests/follows_from.rs
@@ -4,9 +4,11 @@ use opentelemetry_sdk::{
     trace::{SdkTracerProvider, SpanData, SpanExporter, Tracer},
 };
 use std::sync::{Arc, Mutex};
+use std::time::Duration;
 use tracing::Subscriber;
 use tracing_opentelemetry::layer;
 use tracing_subscriber::prelude::*;
+use tracing_subscriber::Layer;
 
 #[derive(Clone, Default, Debug)]
 struct TestExporter(Arc<Mutex<Vec<SpanData>>>);
@@ -56,4 +58,52 @@ fn trace_follows_from_closed() {
     let spans = exporter.0.lock().unwrap();
     // Only the child spans are reported.
     assert_eq!(spans.len(), 2);
+}
+
+#[test]
+fn trace_follows_from_partially_closed() {
+    /// This layer will take 20ms to process
+    #[derive(Clone)]
+    struct WaitOnCloseLayer;
+    impl<S> Layer<S> for WaitOnCloseLayer
+    where
+        S: Subscriber,
+    {
+        fn on_close(&self, _id: tracing::Id, _ctx: tracing_subscriber::layer::Context<'_, S>) {
+            // Some longer running computation
+            std::thread::sleep(Duration::from_millis(20));
+        }
+    }
+
+    // Create a test exporter
+    let (tracer, provider, _exporter, _subscriber) = test_tracer();
+    // And a subscriber with two layers, first our layer, then a slow layer
+    let subscriber = Arc::new(
+        tracing_subscriber::registry()
+            .with(layer().with_tracer(tracer.clone()))
+            .with(WaitOnCloseLayer),
+    );
+
+    // Now triggering a follows_from in the time where the slow layer is processing `close` should still work.
+    tracing::subscriber::with_default(subscriber.clone(), || {
+        let cause_span = tracing::debug_span!("f");
+        let cause_id = cause_span.id().unwrap();
+
+        // mutli-threading similar to `parallel.rs`
+        std::thread::scope(|scope| {
+            // One thread will drop the `follows` span, triggering the removal of `OtelData` extension data, and an artificial 20ms wait
+            scope.spawn(|| {
+                drop(cause_span);
+            });
+            std::thread::sleep(Duration::from_millis(5));
+
+            // Another thread will make a following span based on the stored id, which triggers `on_follows_from`.
+            // However, the wait has not finished and thus the registry context has not been cleared.
+            let s = tracing::debug_span!("span");
+            // This should not panic (but did prior to fix of #183)
+            s.follows_from(cause_id);
+        });
+    });
+
+    drop(provider); // flush all spans
 }
